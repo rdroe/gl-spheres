be1d55a33e36c54b845c5325e24ed3816f842356
rdroe <d@vidroe.com>
2021-05-15 10:41:49 -0500

    when locally served, use pages dir

--- server/Server.py
+++ server/Server.py
@@ -4,9 +4,12 @@ import http.server
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import socketserver
 
+# A bunch of rigmarole to set the directory to "pages"
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory="pages", **kwargs)
+    
 PORT = 8080
-
-Handler = http.server.SimpleHTTPRequestHandler
 
 Handler.extensions_map={
         '.manifest': 'text/cache-manifest',
@@ -20,7 +23,6 @@ Handler.extensions_map={
         '.wasm': 'application/wasm'
     }
 
-httpd = socketserver.TCPServer(("", PORT), Handler)
-
-print("serving at port", PORT)
-httpd.serve_forever()
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
