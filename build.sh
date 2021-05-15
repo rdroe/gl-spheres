e63138fce319ac899af2108fde5b67e550d1308c
rdroe <d@vidroe.com>
2021-05-15 10:06:17 -0500

    github pages

--- build.sh
+++ build.sh
@@ -5,4 +5,9 @@ echo "project root: $proj"
 cd build \
     && cmake -DCMAKE_TOOLCHAIN_FILE="/Users/robertroe/sites/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake"  -DCMAKE_BUILD_TYPE=Debug $proj -G "Unix Makefiles" .. \
     && cmake --build . \
+    && cp *.html ../pages \
+    && cp *.wasm ../pages \
+    && cp *.js ../pages \
+    && cp *.data ../pages \
+    && mv ../pages/main.html ../pages/index.html \
     && cd ..
